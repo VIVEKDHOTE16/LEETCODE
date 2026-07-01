70d85c88b1dd4b4692332adbdd2421db05c72984
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-07-01 14:04:26 +0530

    Time: 0 ms (100%), Space: 13.3 MB (78.91%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int low=0;
+        int high=row*col-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            int rows = mid / col;
+            int cols = mid % col;
+
+            if(matrix[rows][cols]==target){
+                return true;
+            }
+            else if(target<=matrix[rows][cols]){
+                high=mid-1;
+            }
+            else low=mid+1; 
+        }
+        return false;   
+    }
+};
