fcf9d9517f652bf80ddcff784a51921b9013758a
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-08-31 16:28:56 +0530

    Time: 98 ms (19.14%), Space: 56.1 MB (57.54%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int subarraysWithKDistinct(vector<int>& nums, int k) {
+        return maxcount(nums,k) - maxcount(nums,k-1);
+    
+    }
+    int maxcount(vector<int>& nums,int k){
+        int n=nums.size();
+        int l=0;
+        int r=0;
+        int count=0;
+        unordered_map<int,int>mpp;
+        while(r<n){
+            mpp[nums[r]]++;
+            while(mpp.size()>k){
+                mpp[nums[l]]--;
+                if(mpp[nums[l]]==0) mpp.erase(nums[l]);
+                l++;
+            }
+            count+=r-l+1;
+            r++;   
+        }
+        return count;
+     
+    }
+};
