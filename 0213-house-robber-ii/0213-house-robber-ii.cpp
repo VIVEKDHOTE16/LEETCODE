6073bfa7238c3dfcb6bca22dc3406a09f2cd5ce6
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-09-08 23:14:42 +0530

    Time: 0 ms (100%), Space: 10 MB (97.24%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,27 +1,24 @@
 class Solution {
-    int maxnonadjacentsum(vector<int>& numss) {
-        int n = numss.size();
-        int prev2=0;
-        int prev=numss[0];
-        for (int i=1;i<n;i++){
-            int take=numss[i];
-            if(i>1) take+=prev2;
-            int nontake=0+prev;
-            int curr=max(take,nontake);
-            prev2=prev;
-            prev=curr;
-        }
-        return prev;; 
-    }
 public:
+    int helper(vector<int>& nums , int st, int end){
+        int n =nums.size();
+        
+        int prev1=nums[st];
+        int prev2=max(nums[st],nums[st+1]);
+        int result=prev2;
+
+        for(int i=st+2;i<=end;i++){
+            result=max(prev2,prev1+nums[i]);
+            prev1=prev2;
+            prev2=result;
+        }
+        return result;
+    }
     int rob(vector<int>& nums) {
-        vector<int>temp1,temp2;
         int n=nums.size();
         if(n==1) return nums[0];
-        for(int i=0;i<n;i++){
-            if(i != 0) temp1.push_back(nums[i]);
-            if(i != n-1) temp2.push_back(nums[i]);
-        }
-        return max(maxnonadjacentsum(temp1),maxnonadjacentsum(temp2));
+        if(n==2) return max(nums[0],nums[1]);
+
+        return max(helper(nums,0,n-2),helper(nums,1,n-1));
     }
 };
