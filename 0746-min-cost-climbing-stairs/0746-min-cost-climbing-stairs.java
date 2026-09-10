526cc84273a65895e7e3dd47c33397c9bd6e3698
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-09-10 15:07:03 +0530

    Time: 0 ms (100%), Space: 44.6 MB (87.94%) - LeetHub

---
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n= cost.length;
+
+        int prev1=0;
+        int prev2=0;
+        int result=0;
+
+        for(int i=2;i<=n;i++){
+            result=Math.min(prev2+cost[i-1],prev1+cost[i-2]);
+
+            prev1=prev2;
+            prev2=result;
+        }
+        return result;
+        
+    }
+}
