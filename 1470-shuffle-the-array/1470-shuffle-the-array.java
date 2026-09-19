fe17fec097ca0fae9418188f6240bc02c7235852
VIVEK DHOTE <vivekdhote16604@gmail.com>
2026-09-19 14:33:18 +0530

    Time: 0 ms (100%), Space: 46.8 MB (7.3%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int m=nums.length;
+        int j = n;
+        int[] ans= new int[m];
+
+        for (int i = 0; i < n; i++) {
+            ans[2 * i] = nums[i];
+            ans[2 * i + 1] = nums[j];
+
+            j++;
+        }
+
+        return ans;
+    }
+}
